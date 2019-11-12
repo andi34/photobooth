6bd4a040715e2dbd1c5fae9909cd05a5a86e5f4d
Andreas Blaesius <andi@unlegacy-android.org>
2019-11-12 07:44:33 +0100

    Update fr.js

--- resources/lang/fr.js
+++ resources/lang/fr.js
@@ -1,5 +1,6 @@
 /* exported L10N */
 const L10N = {
+    'nextPicture': 'Photo suivante',
     'send': 'Envoyer',
     'show_error_messages': 'Afficher les messages d\'erreur',
     'general_default_imagefilter': 'Filtre d\'image par défaut',

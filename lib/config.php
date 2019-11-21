e630aa524aea7c7bcaf9f54fb0dd10e9dedf2c34
Andreas Blaesius <andi@unlegacy-android.org>
2019-11-21 20:46:33 +0100

    Ignore errors if dev mode is disabled
    By Mhellmeier

--- lib/config.php
+++ lib/config.php
@@ -82,6 +82,8 @@ if ($config['dev']) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+} else {
+    error_reporting(0);
 }
 
 if (is_array($config['color_theme'])) {

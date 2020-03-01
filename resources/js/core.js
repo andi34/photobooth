d0981cd0915c45b6d53cd5b271ff9d978667f2cd
Andreas Blaesius <andi@unlegacy-android.org>
2020-03-01 11:43:38 +0100

    Fix "Added non-passive event listener" Violation
    Solution from http://www.visionfortech.com/2017/07/Added-non-passive-event-listener-to-a-scroll-blocking-touchstart-event-Consider-marking-event-handler-as-passive-to-make-the-page-more-responsive-by-visionfortech.html
    
    Change-Id: I4a1543cf153e446e325a0f3852dff7b82a90abb6

--- resources/js/core.js
+++ resources/js/core.js
@@ -676,6 +676,17 @@ const photoBooth = (function () {
         });
     }
 
+    jQuery.event.special.touchstart = {
+        setup: function(_, ns, handle) {
+            if (ns.includes('noPreventDefault')) {
+                this.addEventListener('touchstart', handle, {passive: false});
+            } else {
+                this.addEventListener('touchstart', handle, {passive: true});
+            }
+        }
+    };
+
+
     return public;
 })();
 

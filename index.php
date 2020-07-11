4ecec49baf5cb61bd53fdebfb65f3ab6468d418a
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-07-11 13:09:41 +0200

    fixed preview

--- index.php
+++ index.php
@@ -142,7 +142,7 @@ $imagelist = ($config['newest_first'] === true) ? array_reverse($images) : $imag
 				<div class="loading"></div>
 			</div>
             <!--<canvas style="position: fixed; width: 100%; height: 100%" width="960" height="640"></canvas>-->
-            <img id="remoteVideo" src="http://localhost:8090/video-stream.mjpeg"/>
+            <video id="remoteVideo" src="http://localhost:8090/video-stream.mjpeg" autoplay playsinline></video>
 		</div>
 
 		<!-- Result Page -->

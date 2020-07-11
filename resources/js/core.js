0e5ee460ae968b47919364cc64d95883e0e6141f
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-07-11 13:20:51 +0200

    fixed preview

--- resources/js/core.js
+++ resources/js/core.js
@@ -126,8 +126,6 @@ const photoBooth = (function () {
 
         Photobooth.previewVideoPlayer = window.setInterval(function () {
             console.log("Updating Image")
-            const img = new Image();
-            img.src = "http://localhost:8090/video-stream.mjpeg"
             ctx.drawImage(img, 0, 0, 960, 640, 0, 0, 960, 640);
         }, 100);
     }
